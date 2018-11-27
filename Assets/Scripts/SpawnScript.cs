c37984ab568390ad8b7b42bde6c55d6bb3391217
ImanSyed <31649603+ImanSyed@users.noreply.github.com>
2018-11-27 09:47:24 +0530

    spawner

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+    public int count = 4;
+    [SerializeField] Vector2 minBounds, maxBounds;
+    [SerializeField] GameObject enemy;
+    EnemyScript[] enemies;
+ 
+
+	void Start () {
+        enemies = new EnemyScript[count];
+        while (count > 0)
+        {
+            GameObject e = Instantiate(enemy, transform.position, transform.rotation);
+            e.GetComponent<EnemyScript>().Initialize(this);
+            enemies[count - 1] = e.GetComponent<EnemyScript>();
+            count--;
+            transform.position = new Vector2(Random.Range(minBounds.x, minBounds.y), Random.Range(maxBounds.x, maxBounds.y));
+            Debug.Log(1);
+        }
+        foreach(EnemyScript es in enemies)
+        {
+            es.spawning = false;
+        }
+	}
+	
+
+	void Update () {
+		
+	}
+}
