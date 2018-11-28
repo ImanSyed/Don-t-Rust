916c0f7cbe07bde7af4977108845179432118663
dhruvj97 <45250840+dhruvj97@users.noreply.github.com>
2018-11-29 01:14:02 +0530

    healthUI

---
+++ Assets/Scripts/Health_Scale.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_Scale : MonoBehaviour
+{
+    public SpriteRenderer hColor;
+    public float hScale = 1;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        hColor.transform.localScale = new Vector3(0, hScale, 0);
+	}
+}
