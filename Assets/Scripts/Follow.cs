17eaa391292b67e4973ed0569cc4d0978b9f8b55
PRANJALSLAPTOP\Pranjal <pranjalnitrox@gmail.com>
2018-11-29 16:15:16 +0530

    Revert "PRESS F TO PAY CAMERA SHAKE RESPECCS"
    This reverts commit 5fe5b2421be279fbd2b33f0485c37d51f5385e45.

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -1,28 +1,19 @@
 ﻿using UnityEngine;
 
-public class Follow : MonoBehaviour
-{
+public class Follow : MonoBehaviour {
 
-	public Transform target;
-	public int shakeMagnitude;
-	public int roughness;
-	public int numberOfShakes;
+    public Transform target;
 
+    [SerializeField] Vector2 minPlane, maxPlane;
 
-	bool shaking = false;
-	float shakeX=0, shakeY=0;
-	float tarX=0, tarY=0;
-	int countdown=0;
-	int shakesLeft=0;
-	[SerializeField] Vector2 minPlane, maxPlane;
-
-	void Update ()
-	{
-		if (target) {
+    void Update()
+    {
+        if (target)
+        {
            
-			Vector3 pos = target.position;
-			pos.z = -10;
-			/*if (target.position.x > minPlane.x && target.position.x < maxPlane.x)
+            Vector3 pos = target.position;
+            pos.z = -10;
+            /*if (target.position.x > minPlane.x && target.position.x < maxPlane.x)
             {
                 pos.x = target.position.x;
             }
@@ -30,52 +21,7 @@ public class Follow : MonoBehaviour
             {
                 pos.y = target.position.y;
             }*/
-			transform.position = pos;
-
-			if (Input.GetKeyDown (KeyCode.F)) {
-				shaking = true;
-				shakesLeft = numberOfShakes;
-				countdown = roughness;
-
-
-			}
-			if (shaking) {
-				tarX = pos.x + shakeMagnitude / 10f;
-				tarY = pos.y + shakeMagnitude / 10f;
-			} else {
-				tarX = pos.x;
-		        tarY = pos.y;
-			}
-
-			if (countdown > 0) countdown--;
-			if (countdown <= 0) {
-				if (shakesLeft > 0) {
-					shakesLeft--;
-					countdown = roughness;
-					tarX = -tarX;
-					tarY = -tarY;
-				} else {
-					countdown = 0;
-					shaking = false;
-				}
-			}
-
-			if (shakeX == 0 && shakeY == 0)
-				shaking = false;
-
-
-			transform.position = new Vector3 (transform.position.x + 0.01f*(tarX-transform.position.x), transform.position.y + 0.01f*(tarY-transform.position.y) , pos.z);
-
-			if (shakeX <= 0)
-				shakeX = 0;
-			else
-				shakeX -= shakeMagnitude / 100f;
-			
-			if (shakeY <= 0)
-				shakeY = 0;
-			else
-				shakeY -= shakeMagnitude / 100f;
-			
-		}
-	}
+            transform.position = pos;
+        }
+    }
 }
