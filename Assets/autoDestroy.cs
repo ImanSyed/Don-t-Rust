0d93ef9998199c086430c824cd99d0ec52b60405
PRANJALSLAPTOP\Pranjal <pranjalnitrox@gmail.com>
2018-11-29 23:48:33 +0530

    autoDestroy for particle added

---
+++ Assets/autoDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class autoDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	private ParticleSystem ps;
+	void Start () {
+		ps = GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ps && !ps.IsAlive ()) 
+		{
+			Destroy (gameObject);
+		}
+		
+	}
+}
